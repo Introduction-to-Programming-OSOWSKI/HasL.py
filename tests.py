deb0d0b06b6dae27dfa11e55fcd3bb992dcd9b71
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2023-03-29 13:53:51 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2021
-month = 10
-day = 8
+year = 2023
+month = 4
+day = 15
 
 def test_code():
     assert main.hasL("cat") == False, 'hasL("cat") == False failed'
